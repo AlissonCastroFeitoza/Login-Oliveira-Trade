78d4482b250995aeb51c2687563e84d29b7236a4
Alisson Castro Feitoza <feitozaac@gmail.com>
2022-12-06 13:44:48 -0300

    adjusting app and setting icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import GlobalStyle from './styles/GlobalStyle';
+import Routes from './Routes/index'
+import {AuthProvider} from './Contexts/AuthContext'
 function App() {
   return (
     <div className="App">
-      
+      <AuthProvider>
+      <GlobalStyle/>
+      <Routes/>
+      </AuthProvider>
     </div>
   );
 }
